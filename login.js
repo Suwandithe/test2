3b36cc856dcb40b5eee49683a79bd544184727f5
Suwandi The <144767691+Suwandithe@users.noreply.github.com>
2023-12-16 21:37:33 +0700

    Add files via upload

---
+++ login.js
@@ -0,0 +1,14 @@
+document.querySelectorAll('input').forEach((item) => {
+    item.addEventListener("focus", function(){
+        item.previousElementSibling.className = "label-selected";
+    })
+
+    item.addEventListener("blur", function(){
+        if(item.value == ''){
+            item.previousElementSibling.className ='';
+        }
+    })
+});
+
+
+
